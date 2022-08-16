692e9c54f34825fd17597863ad1ea25e5c3d89d6
Lizbeth Baez <lizbaez.140799@gmail.com>
2022-08-16 09:37:58 -0700

    Outlet_1Commit

--- Outlets/ViewController.swift
+++ Outlets/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //Un OUTLET es una conexiòn entre un control de UI del storyboard y una variable de Còdigo Swift.
+    @IBOutlet weak var lblMyLabel: UILabel!
+    
+    //Esta funciòn se ejecuta ya que cargo todo en memoria.
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        lblMyLabel.textColor = UIColor.blue
+        
     }
-
+    
 
 }
 
